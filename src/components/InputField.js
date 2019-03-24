0fac338f8d71ccd499e6fdfad9cca094574372b1
Rania Wael <rwael@sumerge.com>
2019-03-24 18:16:06 +0200

    Input Field Component

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -3,16 +3,28 @@ import React, { Component } from 'react';
 class InputField extends Component {
   constructor(props) {
       super(props);
-      this.state = { inputName: 'X' };
-    }
+      this.state = { enteredData: ' ' };
+      this.handleChange = this.handleChange.bind(this);
+  }
 
-    render() {
+  handleChange(event) {
+    this.setState({ enteredData: event.target.value });
+  }
+
+  render() {
     return (
       <div>
-        {this.state.inputName}
+        <label>
+          { this.props.fieldName }
+          <input type = { this.props.fieldType } value = { this.state.enteredData } onChange = { this.handleChange } />
+        </label>
       </div>
     );
   }
 }
+
+const styles= {
+  
+};
 
 export default InputField;
