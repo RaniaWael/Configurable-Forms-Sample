c1190e5bae422cffbe2f4db347b10f175f4fd294
Rania Wael <rwael@sumerge.com>
2019-03-27 14:24:01 +0200

    Formatting

--- src/components/Section.js
+++ src/components/Section.js
@@ -14,22 +14,22 @@ class Section extends Component {
 
     formField (){
       return this.props.fields.map((field) => {
-      switch(field.fieldType) {
-        case 'radio button': return <RadioButtons fieldName = { field.fieldName } choices = { field.radioBtnOptions } />; break;
-        case 'dropdown': return <Dropdown fieldName = { field.fieldName } options = { field.dropdownOptions } />; break;
-        case 'check list': return <Checklist fieldName = { field.fieldName } choices = { field.checkListOptions } />; break;
-        default: return <InputField fieldName = { field.fieldName } fieldType = { field.fieldType } fieldReq = { field.fieldReq } />;
-      }
+        switch(field.fieldType) {
+          case 'radio button': return <RadioButtons fieldName = { field.fieldName } choices = { field.radioBtnOptions } />; break;
+          case 'dropdown': return <Dropdown fieldName = { field.fieldName } options = { field.dropdownOptions } />; break;
+          case 'check list': return <Checklist fieldName = { field.fieldName } choices = { field.checkListOptions } />; break;
+          default: return <InputField fieldName = { field.fieldName } fieldType = { field.fieldType } fieldReq = { field.fieldReq } />;
+        }
     });
     }
 
     render() {
     return (
       <div>
-      <fieldset style = { styles.section }>
-        <legend>{ this.props.sectionName }</legend>
-          { this.formField() }
-      </fieldset>
+        <fieldset style = { styles.section }>
+          <legend>{ this.props.sectionName }</legend>
+            { this.formField() }
+        </fieldset>
       </div>
     );
   }
